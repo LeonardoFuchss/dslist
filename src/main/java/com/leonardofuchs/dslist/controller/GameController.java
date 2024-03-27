b69450a2c9ec992b61b8e03215604917b2acb48c
Leonardo Fuchs <Leonardo.santosfuchs07@gmail.com>
2024-03-27 17:37:44 -0300

    GET Game by id

--- src/main/java/com/leonardofuchs/dslist/controller/GameController.java
+++ src/main/java/com/leonardofuchs/dslist/controller/GameController.java
@@ -1,12 +1,11 @@
 package com.leonardofuchs.dslist.controller;
 
+import com.leonardofuchs.dslist.dto.GameDto;
 import com.leonardofuchs.dslist.dto.GameMinDto;
 import com.leonardofuchs.dslist.entities.Game;
 import com.leonardofuchs.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,9 @@ public class GameController {
     @GetMapping
     public List<GameMinDto> findAll(){
         return gameService.findAll();
+    }
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id){ // O corpo da requisição receberá um id!
+        return gameService.findById(id); // Chama o método findById do gameService passando o id da requisição por parâmetro!
     }
 }
--- src/main/java/com/leonardofuchs/dslist/services/GameService.java
+++ src/main/java/com/leonardofuchs/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.leonardofuchs.dslist.services;
 
+import com.leonardofuchs.dslist.dto.GameDto;
 import com.leonardofuchs.dslist.dto.GameMinDto;
 import com.leonardofuchs.dslist.entities.Game;
 import com.leonardofuchs.dslist.repository.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,17 @@ import java.util.List;
 
 @Service
 public class GameService {
+
     @Autowired
     private GameRepository gameRepository;
-public List<GameMinDto> findAll() {
-    List<Game> result = gameRepository.findAll(); // Retorna todos os dados buscados pelo repository no banco de dados da entidade definida.
-    return result.stream().map(x -> new GameMinDto(x)).toList(); // Transformando a variável result em um DTO (GameMinDto, apenas os dados necessários!)
-}
+
+    public List<GameMinDto> findAll() {
+        List<Game> result = gameRepository.findAll(); // Retorna todos os dados buscados pelo repository no banco de dados da entidade definida.
+        return result.stream().map(x -> new GameMinDto(x)).toList(); // Transformando a variável result em um DTO (GameMinDto, apenas os dados necessários!)
+    }
+    @Transactional()
+    public GameDto findById(Long id) { // PEDE NO PARÂMETRO QUAL ID SERÁ BUSCADO PELA REQUISIÇÃO
+        Game result = gameRepository.findById(id).get(); // BUSCA NA ENTIDADE O ID DA REQUISIÇÃO! ARMAZENA NA VARIÁVEL RESULT!
+        return new GameDto(result); // RETORNA OS DADOS DO DTO ATRAVÉS DO ID BUSCADO NO RESULT!
+    }
 }
