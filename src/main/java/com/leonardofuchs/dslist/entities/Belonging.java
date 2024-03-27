c19f2c2535d2381fa35ff55363fc91b2316218a1
Leonardo Fuchs <Leonardo.santosfuchs07@gmail.com>
2024-03-27 15:51:25 -0300

    Domain model

---
+++ src/main/java/com/leonardofuchs/dslist/entities/Belonging.java
@@ -0,0 +1,40 @@
+package com.leonardofuchs.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging implements Serializable {
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList gameList, Integer position) {
+        id.setGame(game);
+        id.setGameList(gameList); // Dizendo que o id irá receber 2 referências.
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+}
---
+++ src/main/java/com/leonardofuchs/dslist/entities/BelongingPK.java
@@ -0,0 +1,49 @@
+package com.leonardofuchs.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+@Embeddable // Encapsulando 2 atributos em uma classe só.
+public class BelongingPK implements Serializable {
+    @ManyToOne // Anotação de muitos para um
+    @JoinColumn(name = "game_id")
+    private Game game;
+    @ManyToOne
+    @JoinColumn( name = "list_id")
+    private GameList gameList;
+
+    public BelongingPK() {
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(gameList, that.gameList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, gameList);
+    }
+}
